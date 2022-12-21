69a1727a4f7ac047dd6e871fe16dd57a67699b14
Gonzalo Díaz <diazgonzaloelias@gmail.com>
2022-12-21 18:50:02 +0100

    merge files

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,8 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+
+mkdir out/merged
+cat "C57BL_6NJ_1s.trimmed.fastq.gz C57BL_6NJ_2s.trimmed.fastq.gz > C57BL_6NJ.trimmed.fastq.gz" 
+cat "SPRET_EiJ_1s.trimmed.fastq.gz SPRET_EiJ_2s.trimmed.fastq.gz > SPRET_EiJ.trimmed.fastq.gz"
